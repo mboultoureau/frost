c5e58c765eb72797f03902d82461131f93dbd457
boum6233 <mathis.boultoureau@usherbrooke.ca>
2026-01-05 23:48:03 -0500

    refactor: store raw normal

--- Frost/resources/shaders/Light/PS_PointLight.hlsl
+++ Frost/resources/shaders/Light/PS_PointLight.hlsl
@@ -31,7 +31,7 @@ float4 main(PS_Input input) : SV_TARGET
         return float4(currentLight, 1.0f);
     }
 
-    float3 normal = normalize(NormalTexture.Sample(GBufferSampler, input.TexCoord).rgb * 2.0f - 1.0f);
+    float3 normal = normalize(NormalTexture.Sample(GBufferSampler, input.TexCoord).rgb);
     float4 mat = MaterialTexture.Sample(GBufferSampler, input.TexCoord);
     float metal = mat.r;
     float rough = mat.g;
